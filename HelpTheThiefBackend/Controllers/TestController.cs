7a32f59ded56d01521069897c8528005a85e42eb
virtouso <s.moeen.kamali@gmail.com>
2020-09-26 01:23:53 +0330

    first commit

---
+++ HelpTheThiefBackend/Controllers/TestController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelpTheThiefBackend.Controllers
+{
+    public class TestController : Controller
+    {
+        public string Index()
+        {
+            return "test is running right";
+        }
+    }
+}
